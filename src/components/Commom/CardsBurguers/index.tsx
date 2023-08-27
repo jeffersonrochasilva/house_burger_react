5df921a950174622ac25a374d2208c04bdd047fa
Davi Augusto Morais De Sousa <daviaugusto.developer@gmail.com>
2023-08-27 19:10:18 -0300

    fix: ajustes

--- src/components/Commom/CardsBurguers/index.tsx
+++ src/components/Commom/CardsBurguers/index.tsx
@@ -5,41 +5,50 @@ import { Link } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { values } from "../../../store/state";
 
-const CardsBurguers = (props: {
-  image?: any;
+interface IProps {
+  image?: string;
   title?: string;
   value?: string;
   description?: string[];
   item?: any;
   toGoRoute?: string;
-}) => {
+}
+
+const CardsBurguers = ({
+  image,
+  title,
+  description,
+  value,
+  item,
+  toGoRoute,
+}: IProps) => {
   const setSelectItem = useSetRecoilState(values);
 
   const getItem = (item: any): void => {
-    setSelectItem((val) => ({
+    setSelectItem((val: any) => ({
       ...val,
-      title: "",
-      descriptions: [],
-      image: "",
+      title: title,
+      descriptions: description,
+      image: image,
     }));
     console.log(item, "item");
   };
 
   return (
     <Link to={"/selectItem"}>
-      <div className="burguesContainer" onClick={() => getItem(props.item)}>
+      <div className="burguesContainer" onClick={() => getItem(item)}>
         <div className="cardsDescription">
-          <span className="cardsTextTitle">{props.title}</span>
+          <span className="cardsTextTitle">{title}</span>
           <p className="cardsTextDescription">
-            {props.description
-              ? props.description
+            {description
+              ? description
               : "Pão australiano, 2 hamburguer 150 gr, bacon, cheddar, mussarela, molho..."}
           </p>
-          <p className="cardsValue">R$ {props.value}</p>
+          <p className="cardsValue">R$ {value}</p>
         </div>
         <img
           className="cardsImage"
-          src={props.image}
+          src={image}
           alt="texas_gril"
           title="hambubgueer"
         />
